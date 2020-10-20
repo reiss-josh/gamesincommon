c7b7e8d41a45662f1f81b3cd89226209042e772b
reiss-josh <reiss.joshuae@gmail.com>
2020-10-19 17:24:23 -0700

    fixed my code with a bunch of antipatterns

--- src/components/gameButtonHolder.js
+++ src/components/gameButtonHolder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import GameButton from './gameButton.js';
-import {alphabetizeObjects} from '../utilities/generic_utils.js';
+import {alphabetizeObjects, sepMissingParams, joinMissingParams} from '../utilities/generic_utils.js';
+import {getSteamGamesMultiple, getGamesInCommon} from '../utilities/steamAPI_utils.js';
 
 const INITIAL_STATE = {
 	gamesList: [],
@@ -10,25 +11,58 @@ const INITIAL_STATE = {
 class GameButtonHolder extends Component{
   constructor(props){
     super();
-  }
+	}
 
-  coolFunction(beep){
-    console.log(beep);
-  }
+	//todo: make this less of a horrible mess
+	handleGamesList = async(friends, selectedFriends) => {
+		console.log("handling games list...");
+		this.setState({gamesList: []}); //resets list --- should start up a "loading" image around here
+		let currFrns = friends;
+		let currSelected = selectedFriends;
+
+		//determine what data has/hasn't been memoized; retrieve it
+		let missFound = sepMissingParams(currFrns, currSelected, 'gameLibrary', 'steamid');
+		//console.log(missFound);
+
+		//look up the missing data
+		let allLibraries = [];
+		if(missFound.missing.length > 0){ //if there's anything missing...
+			let missingLibraries = await getSteamGamesMultiple(missFound.missing);
+			allLibraries = allLibraries.concat(missingLibraries);
+		};
+		
+		//memoize the missing data, now that we've grabbed it
+		if(allLibraries.length > 0){ //if we found something above...
+			joinMissingParams(missFound.missing, allLibraries, 'gameLibrary', 'steamid');
+		};
+
+		//make sure that we actually use the memoized data
+		allLibraries = allLibraries.concat(missFound.found);
+
+		//get gamesInCommon, now that we have all our data
+		let gamesInCommon = getGamesInCommon(allLibraries);
+		this.setState({gamesList: gamesInCommon}); //update stored games
+		this.setState({gamesButtonArray: this.generateGameButtons(gamesInCommon, 'name')});
+		
+		return gamesInCommon;
+	}
+
+	generateGameButtons(gamesList, alphaparam){
+		return alphabetizeObjects(gamesList, 'name')
+		.map((game, index) => (
+			<GameButton game = {game}/>
+		))
+	}
 
   componentDidMount(){
-    this.setState({gamesList: this.props.gamesList});
+		this.setState({gamesList: this.props.gamesList});
   }
 
   render(){
     if(this.state){
     return(
-			<ul> {
-				alphabetizeObjects(this.state.gamesList, 'name')
-				.map((game, index) => (
-					<GameButton game = {game}/>
-				))
-      } </ul>);
+			<ul> {this.state.gamesButtonArray} </ul>
+			)
     } else {
       return("");
     }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import SelectorButtonHolder from './components/selectorButtonHolder';
 import GameButtonHolder from './components/gameButtonHolder';
-import {alphabetizeObjects, sepMissingParams, joinMissingParams} from './utilities/generic_utils.js';
-import {getSteamFriends, getPlayerSummaries,
-				getSteamGamesMultiple, getGamesInCommon} from './utilities/steamAPI_utils.js';
+import {getSteamFriends, getPlayerSummaries} from './utilities/steamAPI_utils.js';
 
 //need to get this information from a login page instead
 import {PROXY_URL, API_KEY_USER, STEAM_ID_USER} from './jsenv.js';
@@ -14,22 +12,21 @@ const INITIAL_STATE = {
   steamid: STEAM_ID_USER,
 	friends: [],
 	selectedFriends: [],
-	games: [],
 	selectorButton: null,
-	gamesButtons: null,
 };
 
 class FriendsGamesList extends React.Component {
 	constructor(props) {
 		super();
 		this.state = {...INITIAL_STATE};
+		this.gamesButtons = React.createRef();
 
 		this.updateSelectedFriends = this.updateSelectedFriends.bind(this);
   }
 
 	updateSelectedFriends(newSelected) {
 		this.setState({selectedFriends: newSelected});
-		this.handleGamesList();
+		this.gamesButtons.current.handleGamesList(this.state.friends, this.state.selectedFriends);
 	}
 
 	handleFriendsList = async() => {
@@ -55,92 +52,36 @@ class FriendsGamesList extends React.Component {
 		return summariesResult;
 	}
 
-	handleGamesList = async() => {
-		console.log("handling games list...");
-		this.setState({games: []}); //resets list --- should start up a "loading" image around here
-		let currFrns = this.state.friends;
-		let currSelected = this.state.selectedFriends;
-
-		//determine what data has/hasn't been memoized; retrieve it
-		let missFound = sepMissingParams(currFrns, currSelected, 'gameLibrary', 'steamid');
-		//console.log(missFound);
-
-		//look up the missing data
-		let allLibraries = [];
-		if(missFound.missing.length > 0){ //if there's anything missing...
-			let missingLibraries = await getSteamGamesMultiple(missFound.missing);
-			allLibraries = allLibraries.concat(missingLibraries);
-		};
-		
-		//memoize the missing data, now that we've grabbed it
-		if(allLibraries.length > 0){ //if we found something above...
-			joinMissingParams(missFound.missing, allLibraries, 'gameLibrary', 'steamid');
-
-			//i have somehow accidentally made the following unnecessary...
-			//i'm assuming i messed up some deep vs. shallow copying somewhere, which is probably very bad
-			/*
-			//let joined = joinMissingParams(missFound.missing, allLibraries, 'gameLibrary', 'steamid');
-			let ind;
-			joined.forEach(function (elt) {
-				ind = currFrns.map(e => e.steamid).indexOf(elt.steamid);
-				currFrns[ind].gameLibrary = elt.gameLibrary;
-			});
-			this.setState({friends: currFrns});
-			*/
-		};
-
-		//make sure that we actually use the memoized data
-		allLibraries = allLibraries.concat(missFound.found);
-
-		//get gamesInCommon, now that we have all our data
-		let gamesInCommon = getGamesInCommon(allLibraries);
-		this.setState({games: gamesInCommon}); //update stored games
-		
-		if(this.state.gamesButtons){ //need to pass updated games list down
-			let beep = this.state.gamesButtons;
-			beep.coolFunction("hee")
-		}
-		return gamesInCommon;
-	}
-
 	async componentDidMount() {
 		//get the friends list, fill out its properties, then store it
 		if(this.state.steamid == null){
 			return;
 		}
-		await this.handleFriendsList();
-		await this.handleGamesList();
 
-		this.setState({selectorButton: 
+		await this.handleFriendsList();
+
+		if(this.gamesButtons.current){ //need to pass updated games list down
+			await this.gamesButtons.current.handleGamesList(this.state.friends, this.state.selectedFriends); //todo: need to make this work
+		}
+
+		this.setState({selectorButton:
 			<SelectorButtonHolder
 			friendsList = {this.state.friends}
 			alphaParam = {'personaname'}
 			selectedFriends = {this.state.selectedFriends}
 			handler = {this.updateSelectedFriends}/>
 		});
-
-		this.setState({gamesButtons:
-			<GameButtonHolder
-			gamesList = {this.state.games}/>
-		});
 	}
 
 	render() {
-		let currComponent = this; //cache reference for the "setState" in here
 		let friendButtons, gamesButtons;
 
-		//todo: this shouldn't all be happening in render, right?
 		//friend list JSX
-		friendButtons = (this.state.friends && this.state.friends.length > 0) ?
-										this.state.selectorButton : <div>do you not have any friends??</div>;
+		friendButtons = this.state.selectorButton;
 
-		//todo: this shouldn't all be happening in render
 		//games list JSX
-		if(this.state.games && this.state.games.length > 0){
-			gamesButtons = this.state.gamesButtons;
-		} else {
-			gamesButtons = <div>you have no games in common!! is one of your friends' games library private? maybe you haven't selected any users?</div>
-		}
+		gamesButtons = <GameButtonHolder ref = {this.gamesButtons}
+										gamesList = {[]}/>
 
 		if(this.state.steamid){
 			return(
