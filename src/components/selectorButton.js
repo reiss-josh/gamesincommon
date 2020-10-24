0d4a787aadd9228640be101817c9443383088050
reiss-josh <reiss.joshuae@gmail.com>
2020-10-24 13:19:58 -0700

    fixed some css handling

--- src/components/selectorButton.js
+++ src/components/selectorButton.js
@@ -9,6 +9,8 @@ class SelectorButton extends Component {
   constructor(props) {
     super();
     this.state = {...INITIAL_STATE};
+
+    this.onButtonClick = this.onButtonClick.bind(this);
   }
 
   //if this button is the logged-in user's button, default it to Clicked.
@@ -17,21 +19,46 @@ class SelectorButton extends Component {
   }
 
   //when we click the button, update its state and pass that update upwards to its parent.
-  onButtonClick(ref) {
-    let newClickState = !ref.state.isClicked; //store the state we are updating to
-    ref.setState({isClicked: newClickState}); //update our state
-    ref.props.handler(ref.props.person, newClickState); //tell our parent to update our state
+  onButtonClick(e) {
+    if (e) {e.preventDefault()};//prevents the white outline on the button
+    let newClickState = !this.state.isClicked; //store the state we are updating to
+    this.setState({isClicked: newClickState}); //update our state
+    this.props.handler(this.props.person, newClickState); //tell our parent to update our state
+  }
+
+  
+  handleClick(e){
+    if (e) {e.preventDefault()}; //prevents the white outline on the button
+  }
+
+  getBorderColor(ref){
+    let currBorder = ref.state.isClicked ? '#75a72d' : '#a72d2d';
+    return {borderColor: currBorder};
+  }
+
+  handleHover(e) {
+    if(e.target.type === 'submit')
+      e.target.style.background = '#434855'
+  }
+
+  handleExit(e){
+    if(e.target.type === 'submit')
+      e.target.style.background = '#262931';
   }
 
   render(){
-    let isClicked = this.state.isClicked;
     let person = this.props.person;
 
     return (
-      <button className = {isClicked ? 'friend-button-on' : 'friend-button'} onClick={() => {this.onButtonClick(this)}}>
-        <img src={person.avatar} alt = {person.personaname}></img>
-        <span>{person.personaname}</span>
-      </button>
+      <div onMouseOver={this.handleHover}
+        onMouseLeave={this.handleExit}>
+        <button className = {'friend-button'}
+          onMouseDown={this.onButtonClick}
+          style = {this.getBorderColor(this)}>
+          <img src={person.avatar} alt = {person.personaname}></img>
+          <span>{person.personaname}</span>
+        </button>
+      </div>
     )
   }
 }
