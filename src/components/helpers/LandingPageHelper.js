39394155885c72b65ec8964ae9ee772fdb18de08
reiss-josh <reiss.joshuae@gmail.com>
2020-11-30 17:50:13 -0800

    Cleaned up some of the CSS

--- src/components/helpers/LandingPageHelper.js
+++ src/components/helpers/LandingPageHelper.js
@@ -8,38 +8,49 @@ import {isNumeric} from '../utilities/generic_utils'
 class LandingPageHelper extends React.Component {
   constructor(props){
     super();
-    this.state = {text: '', myid: '', submitted: false};
+    this.state = {text: '', myid: ''};
   }
 
   onInput(targValue){
     this.setState({text: targValue});
   }
 
-  async onClick(e){
-    this.setState({submitted: true});
+   onFormSubmit = async(e) => {
+    e.preventDefault();
     let output = await this.props.steamid.handleVanity(this.state.text);
     this.setState({myid: output});
-    if(!isNumeric(output)) this.setState({submitted:false});
     this.props.handler(output);
   }
 
   
   render(){
     return(
-      <div>
-        <span style = {{color:"#d36f6f"}}>{this.state.myid}</span>
-        <div>
-          <input value={this.state.text} placeholder = "Vanity URL or Steam ID" onChange={e => this.onInput(e.target.value)}/>
-          <button onClick = {e => this.onClick(e)}>SUBMIT</button>
+      <div class = "landing-page">
+        <div class = "errorCode">
+          <span style = {{color:"#d36f6f"}}>{this.state.myid}</span>
         </div>
-        <img
-          alt="example of a steam vanity url"
-          src={"https://raw.githubusercontent.com/reiss-josh/gamesincommon/master/src/img/vanityExample.png"}
-        />
-        <img
-          alt="example of a steam id url"
-          src={"https://raw.githubusercontent.com/reiss-josh/gamesincommon/master/src/img/steamidExample.png"}
-        />
+        <div class = "idInputBox">
+          <input type = "text" value={this.state.text} placeholder = "Vanity URL or Steam ID" onChange={e => this.onInput(e.target.value)}/>
+        </div>
+        <div class = "idInputButton">
+          <form onSubmit={this.onFormSubmit}>
+            <button type="submit">SUBMIT</button>
+          </form>
+        </div>
+        <div class = "tutImages">
+          <div class = "vanityImg">
+            <img
+              alt="example of a steam vanity url"
+              src={"https://raw.githubusercontent.com/reiss-josh/gamesincommon/master/src/img/vanityExample.png"}
+            />
+          </div>
+          <div class = "steamidImg">
+            <img
+              alt="example of a steam id url"
+              src={"https://raw.githubusercontent.com/reiss-josh/gamesincommon/master/src/img/steamidExample.png"}
+            />
+          </div>
+        </div>
       </div>
     )
   }
