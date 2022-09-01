9baf472a42a647fe5b2e22b975d6da4a4852c8d0
reiss-josh <reiss.joshuae@gmail.com>
2022-09-01 14:09:55 -0700

    Added title tag

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Head from 'next/head';
 
 import SteamID, { SteamIDContext } from '/src/components/services/SteamID';
 import App from '/src/components/helpers/App';
 
 function HomePage(){
-  return <div><SteamIDContext.Provider value={new SteamID()}><App/></SteamIDContext.Provider></div>
+  return (
+    <div>
+      <Head>
+        <title>Games In Common</title>
+        <meta property="og:title" content="Games In Common" key="title" />
+      </Head>
+      <SteamIDContext.Provider value={new SteamID()}>
+        <App/>
+      </SteamIDContext.Provider>
+    </div>
+  )
 }
 export default HomePage
