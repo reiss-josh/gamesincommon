ed055912bb4a461ab6d12c29044df205b7b47ed7
reiss-josh <reiss.joshuae@gmail.com>
2022-09-01 15:07:33 -0700

    tried to revert changes

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,9 @@
 import React from 'react';
-import Head from 'next/head';
 
 import SteamID, { SteamIDContext } from '/src/components/services/SteamID';
 import App from '/src/components/helpers/App';
 
 function HomePage(){
-  return (
-    <div>
-      <Head>
-        <title>Games In Common</title>
-        <meta property="og:title" content="Games In Common" key="title" />
-      </Head>
-      <SteamIDContext.Provider value={new SteamID()}>
-        <App/>
-      </SteamIDContext.Provider>
-    </div>
-  )
+  return <div><SteamIDContext.Provider value={new SteamID()}><App/></SteamIDContext.Provider></div>
 }
 export default HomePage
